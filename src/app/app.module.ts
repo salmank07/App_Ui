094e1b5cdc684d6eb35e7786d083fa566553d164
salmank07 <mohamedsalmankhan509@gmail.com>
2022-07-08 14:38:29 +0530

    8 jul progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Camera } from '@ionic-native/camera/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy}, Camera],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
