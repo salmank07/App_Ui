bfccba4c8f2cbfdfb7f662f4c2c0e80178ac426b
salmank07 <mohamedsalmankhan509@gmail.com>
2022-07-09 14:16:12 +0530

    commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import { LoadingController, Platform, ToastController } from '@ionic/angular';
+import jsQR from 'jsqr';
+import { Event, Router } from '@angular/router';
 
 
 @Component({
@@ -6,8 +10,183 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements AfterViewInit {
 
-  constructor() { }
+
+
+  @ViewChild('video', { static: false }) video: ElementRef;
+  @ViewChild('canvas', { static: false }) canvas: ElementRef;
+  @ViewChild('fileinput', { static: false }) fileinput: ElementRef;
+
+  slideOpts = {
+    preloadImages: true,
+    spaceBetween: 10,
+    slidesPerView: 1,
+    speed: 500,
+    autoplay: true
+  };
+
+  canvasElement: any;
+  videoElement: any;
+  canvasContext: any;
+  scanActive = false;
+  scanResult = null;
+  loading: HTMLIonLoadingElement = null;
+
+  constructor(
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private plt: Platform,
+    private router: Router
+  ) {
+    const isInStandaloneMode = () =>
+      'standalone' in window.navigator && window.navigator['standalone'];
+    if (this.plt.is('ios') && isInStandaloneMode()) {
+      console.log('I am a an iOS PWA!');
+      // E.g. hide the scan functionality!
+    }
+
+
+    this.product = [
+      { productId: '1', product: 'iphone 13', category: 'mobile', rating: 4, src: '../../assets/images/26.jpg' },
+      { productId: '2', product: 'Panasonic Oven', category: 'Electronics', rating: 2, src: '../../assets/images/p2.jpg' },
+      { productId: '3', product: 'Vittara Brezza', category: 'Automobile', rating: 3, src: '../../assets/images/28 - Copy.jpg' },
+      { productId: '4', product: 'Nestle Maggi', category: 'Food', rating: 2.5, src: '../../assets/images/27.jpg' }
+    ];
+  }
+
+  move(params: any) {
+    console.log(params, "id")
+    localStorage.setItem('productId', params);
+    this.router.navigate(['/tabs/tab2']);
+  }
+
+  product: any
+  ngAfterViewInit() {
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasContext = this.canvasElement.getContext('2d');
+    this.videoElement = this.video.nativeElement;
+  }
+
+  // Helper functions
+  async showQrToast() {
+    const toast = await this.toastCtrl.create({
+      message: `Open ${this.scanResult}?`,
+      position: 'top',
+      buttons: [
+        {
+          text: 'Open',
+          handler: () => {
+            window.open(this.scanResult, '_system', 'location=yes');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+
+  reset() {
+    this.scanResult = null;
+  }
+
+  stopScan() {
+    this.scanActive = false;
+  }
+
+
+  async startScan() {
+    // Not working on iOS standalone mode!
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment' }
+    });
+
+    this.videoElement.srcObject = stream;
+    // Required for Safari
+    this.videoElement.setAttribute('playsinline', true);
+
+    this.loading = await this.loadingCtrl.create({});
+    await this.loading.present();
+
+    this.videoElement.play();
+    requestAnimationFrame(this.scan.bind(this));
+  }
+
+  async scan() {
+    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
+      if (this.loading) {
+        await this.loading.dismiss();
+        this.loading = null;
+        this.scanActive = true;
+      }
+
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
+
+      this.canvasContext.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+
+      if (code) {
+        this.scanActive = false;
+        this.scanResult = code.data;
+        this.showQrToast();
+      } else {
+        if (this.scanActive) {
+          requestAnimationFrame(this.scan.bind(this));
+        }
+      }
+    } else {
+      requestAnimationFrame(this.scan.bind(this));
+    }
+  }
+
+
+  captureImage() {
+    this.fileinput.nativeElement.click();
+  }
+
+  handleFile(ev: any) {
+    const file = ev.target.files.item(0);
+
+
+    var img = new Image();
+    img.onload = () => {
+      this.canvasContext.drawImage(img, 0, 0, this.canvasElement.width, this.canvasElement.height);
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+
+      if (code) {
+        this.scanResult = code.data;
+        this.showQrToast();
+      }
+    };
+    img.src = URL.createObjectURL(file);
+  }
+
+
+
+
 
 }
+
+
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,35 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor() {
+
+
+    this.products = [
+      { productId: '1', product: 'iphone 13', category: 'mobile', rating: 4 },
+      { productId: '2', product: 'Panasonic Oven', category: 'Electronics', rating: 2 },
+      { productId: '3', product: 'Vittara Brezza', category: 'Automobile', rating: 3 },
+      { productId: '4', product: 'Nestle Maggi', category: 'Food', rating: 2.5 }
+    ];
+
+    this.clicked();
+  }
+  ionViewWillEnter() {  
+    this.clicked();
+
+  }
+  products: any
+  gotit: any
+  clickedProducts:any;
+
+  clicked() {
+    this.clickedProducts = localStorage.getItem('productId');
+    for (let i = 0; i < this.products.length; i++) {
+      if (this.clickedProducts == this.products[i].productId) {
+        this.gotit = this.products[i].rating;
+        break;
+      }
+    }
+    console.log(this.gotit, "jhsdfjhksjkh")
+  }
 
 }
