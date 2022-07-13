189f2e8846135f81f7b8ac6e05a44d242adc89e0
salman <mohamedsalmankhan509>
2022-07-13 15:42:57 +0530

    changed card respv

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -24,6 +24,7 @@ export class Tab1Page {
     autoplay: true
   };
 
+  product: any
   canvasElement: any;
   videoElement: any;
   canvasContext: any;
@@ -37,8 +38,8 @@ export class Tab1Page {
     private plt: Platform,
     private router: Router,
     private scanner: BarcodeScanner
-    
-    
+
+
   ) {
 
     this.scan();
@@ -59,43 +60,11 @@ export class Tab1Page {
     this.router.navigate(['/tabs/tab2']);
   }
 
-  product: any
-
-  // scan() {
-  //   this.qrScanner.prepare()
-  //     .then((status: QRScannerStatus) => {
-  //       if (status.authorized) {
-  //         // camera permission was granted
-
-
-  //         // start scanning
-  //         let scanSub = this.qrScanner.scan().subscribe((text: string) => {
-  //           console.log('Scanned something', text);
-
-  //           this.qrScanner.hide(); // hide camera preview
-  //           scanSub.unsubscribe(); // stop scanning
-  //         });
-
-  //       } else if (status.denied) {
-  //         // camera permission was permanently denied
-  //         // you must use QRScanner.openSettings() method to guide the user to the settings page
-  //         // then they can grant the permission from there
-  //       } else {
-  //         // permission was denied, but not permanently. You can ask for permission again at a later time.
-  //       }
-  //     })
-  //     .catch((e: any) => console.log('Error is', e));
-  // }
-
   scan() {
     this.scanner.scan().then(barcodeData => {
       console.log('Barcode data', barcodeData);
-     }).catch(err => {
-         console.log('Error', err);
-     });
+    }).catch(err => {
+      console.log('Error', err);
+    });
   }
-
-
-
-
 }
