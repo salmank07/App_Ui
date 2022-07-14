2e56b1f55366491523716fa84bd45970f6313c3d
salman <mohamedsalmankhan509>
2022-07-14 15:10:35 +0530

    july

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -14,30 +14,16 @@ export class LoginPage implements OnInit {
   constructor(
     private router: Router,
     private modalcontroller: ModalController
-  ) { 
-    console.log(this.isModalCanDismiss, "jhjhjhjhj")
-  }
+  ) { }
 
-  isModalCanDismiss: boolean;
   isopen: boolean = true;
 
   ngOnInit() { }
 
   logIn() {
-    this.isModalCanDismiss = true;
-    console.log(this.isModalCanDismiss, "kjhhhkhj");
     this.modalcontroller.dismiss();
     this.router.navigate(['/tabs/tab1']);
   }
-  ondismiss() {
-    this.isModalCanDismiss = !this.isModalCanDismiss;
-    this.isopen = !this.isopen;
-  }
-  present() {
-    this.isopen = !this.isopen
 
-  }
-
-  
 }
 
